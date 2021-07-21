f5237f423f89e84405ae31ac604df3c6ea0dd5d2
lbaitemple <lbai@temple.edu>
2021-07-21 11:06:44 -0400

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,28 @@
+import subprocess, time, sys
+from gtts import gTTS
+
+
+def speak(sentence, language='en', slow =False):
+    # search in the excel file to find name based on id in the excel file
+    speech = gTTS(text = sentence, lang = language, slow = False)
+    speech.save("text.mp3")
+    subprocess.call(["afplay", "text.mp3"])
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    while True:
+        try:
+            sentence=input("enter a sentence #:")
+            speak(sentence, language='en')
+            # do something time-cosnuming
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+            print("Bye")
+            sys.exit()
+
+    sys.exit()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
