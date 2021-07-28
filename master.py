a1f38bdf066f08c4204796d9673957d381d9690a
lbaitemple <lbai@temple.edu>
2021-07-28 15:16:26 -0400

    Add files via upload

---
+++ master.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import time, sys
+from gtts import gTTS
+import subprocess, time, sys
+from rfid import parseRFID
+
+
+def showDetails(id, df):
+    found = False
+    for index, row in df.iterrows():
+        if id == row['ID']:
+            found = True
+            sentence = " {} ".format(row['Name'])
+            speak(sentence, language=row['Language'])
+            break
+    if found == False:
+        sentence ="No ID found"
+        speak(sentence, language='en')
+        return False
+
+def getLanguage(id, df):
+    for index, row in df.iterrows():
+        if id == row['ID']:
+            return row['Language']
+            break
+
+def speak(sentence, language, slow = False):
+    speech = gTTS(text = sentence, lang = language, slow = False)
+    speech.save("text.mp3")
+    subprocess.call(["mpg321", "text.mp3"])
+ 
+
+if __name__ == '__main__':
+    
+    df = pd.read_excel(r'ExcelPanda1.xlsx', sheet_name='Sheet1')
+    totalRows = len(df.index)
+    while True:
+        try:
+            #id = input("enter an ID #:")
+            id =parseRFID()
+            print(id)
+            if id.isnumeric():
+                sentence = showDetails(int(id), df)
+            else:
+                print("Please enter a number")
+            time.sleep(0.5)
+
+        except KeyboardInterrupt:
+            print("Bye")
+            sys.exit()
+    sys.exit()
+
+
+
---
+++ rfid.py
@@ -0,0 +1,74 @@
+import smartcard
+#from smartcard.util import toHexString
+import time
+import numpy as np
+
+def parseRFID():
+    while True:
+        time.sleep(0.5)
+        try:
+            reader = smartcard.System.readers()
+            if not reader:
+                print("No readers")
+                return None
+            else:
+                conn =  reader[0].createConnection()
+                conn.connect()
+                #[0xFF, 0x82, 0x00, 0x00, 0x06,KEY(6 bytes)]
+                LOADKEY = [0xFF, 0x82, 0x00, 0x00, 0x06,255,255,255,255,255,255]
+                response = conn.transmit(LOADKEY)
+                if response[1] == 144:
+                    #print("Key A loaded successfully")
+                    time.sleep(1)
+                    #auth block
+                    # COMMAND = [0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, BLOCK, KEY-TYPE, 0x00]
+                    COMMAND = [0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, 0x00, 0x60, 0x00]
+                    response = conn.transmit(COMMAND)
+                    #print(response)
+                    time.sleep(2)
+                    #[0xFF, 0xB0, 0x00, BLOCK-NUMBER, 0x10]
+            #        read = [0xFF, 0xB0, 0x00, 0x00, 0x10] #Read block 0
+                    read = [0xFF, 0xB0, 0x00, 0x04, 0x10] #Read block 0
+                    data=''
+                    try:
+                        data, sw1, sw2 = conn.transmit(read)
+                        #data, sw1, sw2 = cs.connection.transmit(getuid)
+                    except smartcard.Exceptions.NoCardException:
+                        print("")
+                    except smartcard.Exceptions.CardConnectionException:
+                        print("")
+                    #pdata=toHexString(data) 
+                    if (len(data)>0):
+                        indx=9
+                        str=''
+                        while (indx<=16):
+                            if (data[indx]==254):
+                                break
+                            else:
+                                str=str+chr(data[indx])
+
+                            indx = indx +1
+
+                        return str
+                else:
+                    return None
+        except smartcard.Exceptions.NoCardException:
+            print("")
+        except smartcard.Exceptions.CardConnectionException:
+            print("")
+            
+if __name__ == '__main__':
+
+    while True:
+        try:
+            data=parseRFID()
+            print(data)
+        except smartcard.Exceptions.NoCardException:
+            print("")
+        except smartcard.Exceptions.CardConnectionException:
+            print("")        
+        except KeyboardInterrupt:
+            print("Bye")
+            sys.exit()
+    sys.exit()
+
